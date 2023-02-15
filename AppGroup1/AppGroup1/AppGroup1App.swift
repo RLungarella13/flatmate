3412a7e01c67569c1b53bde297a23f6cedb9c7cd
Raffaele Lungarella <r.lungarella@studenti.unisa.it>
2023-02-15 10:06:36 +0100

    Daje Roma

---
+++ AppGroup1/AppGroup1/AppGroup1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppGroup1App.swift
+//  AppGroup1
+//
+//  Created by Raffaele Lungarella on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AppGroup1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
