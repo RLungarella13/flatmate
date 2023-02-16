57f0e3df0d5ed60d543995b5a0f386e5e51c263a
giammyfonty <giammarcofontana@libero.it>
2023-02-16 17:14:48 +0100

    voloooo

--- AppGroup1/AppGroup1/ContentView.swift
+++ AppGroup1/AppGroup1/ContentView.swift
@@ -4,27 +4,44 @@
 //
 //  Created by Raffaele Lungarella on 15/02/23.
 //
-import SwiftUI
-
-//
-//  ContentView.swift
-//  AppGroup1
-//
-//  Created by Raffaele Lungarella on 15/02/23.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            
             VStack {
-                // Contenuto della vista
+                
+                ClickableHStack()
+                    .padding(.horizontal, 16) // aggiungere padding ai lati dell'HStack
+                    .padding(.top, 8) // aggiungere spazio sopra l'HStack
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 4)
+                    .padding(.horizontal, 16)
+                    .padding(.top, 16)
+                    
+                Text("230 €")
+                    .font(.system(size: 30))
+                    .bold()
+                    
+                Text("Balance")
+                    .foregroundColor(.gray)
+                    .bold()
+                    .font(.system(size: 18))
+                
+                
+                Spacer()
+                    
             }
+            .padding(15.0)
+            
             .navigationBarTitle("Cashflow")
             .navigationBarTitleDisplayMode(.automatic)
             .font(.system(size: 30, weight: .bold))
             .foregroundColor(.black)
+            
             
             .navigationBarItems(trailing:
                                     Button(action: {
@@ -33,11 +50,33 @@ struct ContentView: View {
                 Image(systemName: "magnifyingglass")
             }
             )
-            
-            
         }
     }
 }
+
+
+struct ClickableHStack: View {
+    var body: some View {
+        Button(action: {
+            // Azione da eseguire quando l'HStack viene toccato
+        }) {
+            
+            HStack {
+                Spacer()
+                Image(systemName: "person")
+                Image(systemName: "person")
+                Image(systemName: "person")
+                Image(systemName: "person")
+                Spacer()
+            
+            }
+            .padding()
+        }
+    }
+}
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
