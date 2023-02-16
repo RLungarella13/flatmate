ffec3a9fd2d43f5446e9e5f9ea9e2eaf0f788861
giammyfonty <giammarcofontana@libero.it>
2023-02-16 16:24:01 +0100

    gg

--- AppGroup1/AppGroup1/ContentView.swift
+++ AppGroup1/AppGroup1/ContentView.swift
@@ -4,6 +4,14 @@
 //
 //  Created by Raffaele Lungarella on 15/02/23.
 //
+import SwiftUI
+
+//
+//  ContentView.swift
+//  AppGroup1
+//
+//  Created by Raffaele Lungarella on 15/02/23.
+//
 
 import SwiftUI
 
@@ -11,19 +19,26 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-              
+                // Contenuto della vista
             }
             .navigationBarTitle("Cashflow")
+            .navigationBarTitleDisplayMode(.automatic)
+            .font(.system(size: 30, weight: .bold))
+            .foregroundColor(.black)
+            
             .navigationBarItems(trailing:
-                Button(action: {
-                    // Gestione azione del pulsante di ricerca
-                }) {
-                    Image(systemName: "magnifyingglass")
-                }
+                                    Button(action: {
+                // Gestione dell'azione del pulsante di ricerca
+            }) {
+                Image(systemName: "magnifyingglass")
+            }
             )
+            
+            
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
